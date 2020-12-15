769cfb85a212009e80c310a2859258b6c03c8d99
Miranda J. Ignacio <mirandajignacio@gmail.com>
2020-12-15 19:53:10 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,36 +5,33 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
 );
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-  workbox.core.setCacheNameDetails({
-    prefix: "service-worker-math-random",
-    suffix: "v0.0.1",
-    precache: "precache",
-    runtime: "runcache",
-  });
+self.addEventListener('install', event => {
+  if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+    workbox.core.setCacheNameDetails({
+      prefix: "service-worker-math-random",
+      suffix: "v0.0.1",
+      precache: "precache",
+      runtime: "runcache",
+    });
+  
+    console.log(workbox.core.cacheNames.precache);
+  
+    workbox.routing.registerRoute(
+      /^https:\/\/fonts\.googleapis\.com/,
+      new workbox.strategies.StaleWhileRevalidate({
+        cacheName: "google-fonts-stylesheets",
+      })
+    );
 
-  console.log(workbox.core.cacheNames.precache);
-
-  workbox.routing.registerRoute(
-    /^https:\/\/fonts\.googleapis\.com/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: "google-fonts-stylesheets",
-    })
-  );
-  workbox.routing.registerRoute(
-    /\.(?:css)$/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: "styles",
-    })
-  );
-  workbox.routing.registerRoute(
-    /^https:\/\/pokemondb\.net/,
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: "pokemondb",
-    })
-  );
-
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
+    workbox.routing.registerRoute(
+      /\.(?:css)$/,
+      new workbox.strategies.StaleWhileRevalidate({
+        cacheName: "styles",
+      })
+    );
+  
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
+  }
+});
